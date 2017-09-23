1b6701da8c187f71d3ed8ef8dabb0637a0a8a890
Perry Azevedo <perry.azevedo@gmail.com>
2017-09-23 12:45:18 -0500

    added background video

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="bgScreen"></div>
+        <video playsInline loop autoPlay poster="" id="bgVideo" className="bg-video">
+          <source src={process.env.PUBLIC_URL + '/video/video1.webm'} type="video/webm"></source>
+          <source src={process.env.PUBLIC_URL + '/video/video1.mp4'} type="video/mp4"></source>
+        </video>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="Prota Labs" />
         </div>
